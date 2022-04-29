8de44f53a068c4f8ae133c8b35cb8eda7e06f6d2
MatriXcel <wasiimouro@gmail.com>
2022-04-29 19:48:13 -0400

    added new files

--- getdone.py
+++ getdone.py
@@ -151,7 +151,7 @@ torch.cuda.empty_cache()
 max_seq_length=128
 overwrite_cache=True
 preprocessing_num_workers = 8
-batch_size=64
+batch_size=32
 num_train_epochs=25
 device='cuda'
 learning_rate=1e-2
